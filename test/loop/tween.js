4b89d84d989148e07030cd7d208971a91d69850a
Nicolas Gryman <ngryman@gmail.com>
2012-09-18 21:20:53 -0400

    tweening ying!

---
+++ test/loop/tween.js
@@ -0,0 +1,135 @@
+require('chai').should();
+global.window = global;
+// TODO: simulate browser env here
+
+var Tween = require('../../lib').Tween;
+var Ease = require('../../lib').Ease;
+
+describe('Tween', function() {
+    var tweenable = {
+        opacity: 0,
+        volume: 1
+    };
+
+    describe('constructor', function() {
+        it('should initialize tween properly', function() {
+            var tween = new Tween(tweenable, 'opacity', 0, 42, 1337, 'inOut');
+            tween.from.should.eql(0);
+            tween.to.should.eql(42);
+            tween.duration.should.eql(1337);
+            tween.easing.should.eql(Ease.inOut);
+        });
+
+        it('should default easing to linear', function() {
+            var tween = new Tween(tweenable, 'opacity', 0, 1, 400);
+            tween.easing.should.equal(Ease.linear);
+        });
+
+        it('should default duration to 400ms (jQuery default)', function() {
+            var tween = new Tween(tweenable, 'opacity', 0, 1);
+            tween.duration.should.equal(400);
+        });
+
+        it('should default to to 1', function() {
+            var tween = new Tween(tweenable, 'opacity', 0);
+            tween.to.should.equal(1);
+        });
+
+        it('should default from to 0', function() {
+            var tween = new Tween(tweenable, 'opacity');
+            tween.from.should.equal(0);
+        });
+    });
+
+    describe('#start', function() {
+        it('should set property to from value', function() {
+            var tween = new Tween(tweenable, 'opacity volume', 0, 1, 400, 'in');
+            tween.start();
+            tweenable.opacity.should.eql(0);
+            tweenable.volume.should.eql(0);
+
+            tween = new Tween(tweenable, 'opacity volume', 1, 0, 400, 'in');
+            tween.start();
+            tweenable.opacity.should.eql(1);
+            tweenable.volume.should.eql(1);
+        });
+    });
+
+    describe('#tick', function() {
+        it('should update properties correctly', function() {
+            var tween = new Tween(tweenable, 'opacity volume', 0, 1, 400, 'linear');
+            tween.start();
+            tween.tick({ delta: 200 });
+            tweenable.opacity.should.eql(0.5);
+            tweenable.volume.should.eql(0.5);
+
+            tween.tick({ delta: 200 });
+            tweenable.opacity.should.eql(1);
+            tweenable.volume.should.eql(1);
+
+            tween.tick({ delta: 100 });
+            tweenable.opacity.should.be.gt(1);
+            tweenable.volume.should.be.gt(1);
+        });
+
+        it('should update properties correctly when start is above 0', function() {
+            var tween = new Tween(tweenable, 'opacity volume', 1, 2, 400, 'linear');
+            tween.start();
+            tween.tick({ delta: 200 });
+            tweenable.opacity.should.eql(1.5);
+            tweenable.volume.should.eql(1.5);
+
+            tween.tick({ delta: 200 });
+            tweenable.opacity.should.eql(2);
+            tweenable.volume.should.eql(2);
+
+            tween.tick({ delta: 100 });
+            tweenable.opacity.should.be.gt(2);
+            tweenable.volume.should.be.gt(2);
+        });
+
+        it('should update properties correctly when increment is negative', function() {
+            var tween = new Tween(tweenable, 'opacity volume', 1, 0, 400, 'linear');
+            tween.start();
+            tween.tick({ delta: 200 });
+
+            tweenable.opacity.should.eql(0.5);
+            tweenable.volume.should.eql(0.5);
+
+            tween.tick({ delta: 200 });
+            tweenable.opacity.should.eql(0);
+            tweenable.volume.should.eql(0);
+
+            tween.tick({ delta: 100 });
+            tweenable.opacity.should.be.lt(0);
+            tweenable.volume.should.be.lt(0);
+        });
+
+        it('should return true when tween has reach the end', function() {
+            var tween = new Tween(tweenable, 'opacity volume', 0, 1, 400, 'linear');
+            tween.start();
+            tween.tick({ delta: 400 }).should.be.true;
+
+            tween.tick({ delta: 100 }).should.be.true;
+        });
+    });
+
+    describe('#end', function() {
+        it('should set properties to to value', function() {
+            var tween = new Tween(tweenable, 'opacity volume', 0, 1337, 400, 'linear');
+            tween.start();
+            tween.tick({ delta: 500 });
+            tween.end();
+            tweenable.opacity.should.eql(1337);
+            tweenable.volume.should.eql(1337);
+        });
+
+        it('should return true when tween has reach the end', function() {
+            var tween = new Tween(tweenable, 'opacity volume', 0, 1, 400, 'linear');
+            tween.start();
+            tween.tick({ delta: 400 }).should.be.true;
+
+            tween.tick({ delta: 100 }).should.be.true;
+        });
+    });
+});
