a94540bc3e0df005c40020c84f079775765b7a96
Nicolas Gryman <ngryman@gmail.com>
2012-10-19 13:15:07 -0400

    enhanced Application#when accepting different types of params, added Application#for. also some utils functions were added.

--- test/application.js
+++ test/application.js
@@ -11,7 +11,7 @@ describe('Application', function() {
 	var shared = new Application();
 
 	describe('#when', function() {
-		it('should only create the backed state given event and state names', function() {
+		it('should only create the backed state given event and state name', function() {
 			var app = new Application();
 			app.when('init', 'test');
 			app._states.should.have.deep.property('test.state').be.instanceof(State);
@@ -24,6 +24,35 @@ describe('Application', function() {
 			app._states.should.have.deep.property('test.state').and.be.instanceof(State);
 			app._states.test.state.should.have.deep.property('init', State.prototype.init);
 			app._states.test.state.should.have.deep.property('cleanup', State.prototype.cleanup);
+		});
+
+		it('should accept multiple state names', function() {
+			var app = new Application();
+			app.when('init cleanup', 'state1 state2');
+			app._states.should.have.deep.property('state1.state').and.be.instanceof(State);
+			app._states.state1.state.should.have.deep.property('init', State.prototype.init);
+			app._states.state1.state.should.have.deep.property('cleanup', State.prototype.cleanup);
+			app._states.should.have.deep.property('state1.state').and.be.instanceof(State);
+			app._states.state2.state.should.have.deep.property('init', State.prototype.init);
+			app._states.state2.state.should.have.deep.property('cleanup', State.prototype.cleanup);
+		});
+
+		it('should accept an object as events', function() {
+			var app = new Application();
+			var init = function() {};
+			var enterExit = function() {};
+			app.when({
+				init: init,
+				'enter exit': enterExit
+			}, 'state1 state2');
+			app._states.should.have.deep.property('state1.state').and.be.instanceof(State);
+			app._states.state1.state.should.have.deep.property('init', init);
+			app._states.state1.state.should.have.deep.property('enter', enterExit);
+			app._states.state1.state.should.have.deep.property('exit', enterExit);
+			app._states.should.have.deep.property('state1.state').and.be.instanceof(State);
+			app._states.state2.state.should.have.deep.property('init', init);
+			app._states.state2.state.should.have.deep.property('enter', enterExit);
+			app._states.state2.state.should.have.deep.property('exit', enterExit);
 		});
 
 		it('should override with the given delegate given event and state names', function() {
@@ -69,15 +98,117 @@ describe('Application', function() {
 			var called = true;
 			var app = new Application();
 			app.init('state1', function() {})
-			.when('enter exit', 'state1', function() {})
-			.when('focus blur', 'state1', function() {})
-			.transition('state1', function(event, value) {
-				if (called) return;
-				called = true;
-				value.should.eql(0);
-			}, 'out');
+				.when('enter exit', 'state1', function() {})
+				.when('focus blur', 'state1', function() {})
+				.transition('state1', function(event, value) {
+					if (called) return;
+					called = true;
+					value.should.eql(0);
+				}, 'out');
 			app.init('state2', function() {})
-			.when('enter exit', 'state2', function() {}).when('focus blur', 'state2', function() {});
+				.when('enter exit', 'state2', function() {}).when('focus blur', 'state2', function() {});
+			app.change('state1', done);
+		});
+	});
+
+	describe('#for', function() {
+		it('should only create the backed state given event and a state name', function() {
+			var app = new Application();
+			app.for('test', 'init');
+			app._states.should.have.deep.property('test.state').be.instanceof(State);
+			app._states.test.state.should.have.deep.property('init', State.prototype.init);
+		});
+
+		it('should only create the backed state given multiple events name and a state name', function() {
+			var app = new Application();
+			app.for('test', 'init cleanup');
+			app._states.should.have.deep.property('test.state').and.be.instanceof(State);
+			app._states.test.state.should.have.deep.property('init', State.prototype.init);
+			app._states.test.state.should.have.deep.property('cleanup', State.prototype.cleanup);
+		});
+
+		it('should accept multiple state names', function() {
+			var app = new Application();
+			app.for('state1 state2', 'init cleanup');
+			app._states.should.have.deep.property('state1.state').and.be.instanceof(State);
+			app._states.state1.state.should.have.deep.property('init', State.prototype.init);
+			app._states.state1.state.should.have.deep.property('cleanup', State.prototype.cleanup);
+			app._states.should.have.deep.property('state1.state').and.be.instanceof(State);
+			app._states.state2.state.should.have.deep.property('init', State.prototype.init);
+			app._states.state2.state.should.have.deep.property('cleanup', State.prototype.cleanup);
+		});
+
+		it('should accept an object as events', function() {
+			var app = new Application();
+			var init = function() {};
+			var enterExit = function() {};
+			app.for('state1 state2', {
+				init: init,
+				'enter exit': enterExit
+			});
+			app._states.should.have.deep.property('state1.state').and.be.instanceof(State);
+			app._states.state1.state.should.have.deep.property('init', init);
+			app._states.state1.state.should.have.deep.property('enter', enterExit);
+			app._states.state1.state.should.have.deep.property('exit', enterExit);
+			app._states.should.have.deep.property('state1.state').and.be.instanceof(State);
+			app._states.state2.state.should.have.deep.property('init', init);
+			app._states.state2.state.should.have.deep.property('enter', enterExit);
+			app._states.state2.state.should.have.deep.property('exit', enterExit);
+		});
+
+		it('should override with the given delegate given event and state names', function() {
+			var app = new Application();
+			var delegate = function() { return "hi, I'm Mister delegate" };
+			app.for('test', 'init', delegate);
+			app._states.should.have.deep.property('test.state').and.be.instanceof(State);
+			app._states.test.state.should.have.deep.property('init', delegate);
+		});
+
+		it('should override with the given delegate given multiple events name and a state name', function() {
+			var delegate = function() { return "hi, I'm Mister delegate" };
+			shared.for('test', 'init cleanup', delegate);
+			shared._states.should.have.deep.property('test.state').and.be.instanceof(State);
+			shared._states.test.state.should.have.deep.property('init', delegate);
+			shared._states.test.state.should.have.deep.property('cleanup', delegate);
+		});
+
+		it('should override when redefining existing events for the same state', function() {
+			var delegate = function() { return "hi, I'm Miss delegatess" };
+			shared.for('test', 'init cleanup', delegate);
+			shared._states.test.state.should.have.deep.property('init', delegate);
+			shared._states.test.state.should.have.deep.property('cleanup', delegate);
+		});
+
+		it('should pass the event name as argument', function(done) {
+			var app = new Application();
+			app.for('test', 'enter exit', function(event) {
+				event.should.eql('enter');
+				done();
+			});
+			app.change('test');
+		});
+
+		it('should accept additional transition parameter for transition', function() {
+			var app = new Application();
+			var called = 0;
+			app.for('test', 'transition', function() { called++ }, 'inOut');
+			app._states.test.trans.should.be.instanceof(Transition);
+		});
+
+		it('should pass the from value at the first call of transition event', function(done) {
+			var called = true;
+			var app = new Application();
+			app.init('state1', function() {})
+				.for('state1', 'enter exit', function() {})
+				.for('state1', 'focus blur', function() {})
+				.transition('state1', function(event, value) {
+					if (called) return;
+					called = true;
+					value.should.eql(0);
+				}, 'out');
+			app.init('state2', function() {})
+				.for('state2', 'enter exit', function() {})
+				.for('state2', 'focus blur', function() {});
 			app.change('state1', done);
 		});
 	});
--- test/util.js
+++ test/util.js
@@ -58,4 +58,105 @@ describe('util', function() {
 			delete global.SpecialTransition;
 		});
 	});
+
+	describe('.commonAssignement', function() {
+		it('should take a simple word', function() {
+			var assignement = util.commonAssignement('hello', 42);
+			assignement.should.be.a('object');
+			assignement.should.have.property('hello', 42);
+		});
+
+		it('should take multiple words', function() {
+			var assignement = util.commonAssignement('hello how are you', 42);
+			assignement.should.be.a('object');
+			assignement.should.have.property('hello', 42);
+			assignement.should.have.property('how', 42);
+			assignement.should.have.property('are', 42);
+			assignement.should.have.property('you', 42);
+		});
+
+		it('should take an array', function() {
+			var assignement = util.commonAssignement(['hello', 'how', 'are', 'you'], 42);
+			assignement.should.be.a('object');
+			assignement.should.have.property('hello', 42);
+			assignement.should.have.property('how', 42);
+			assignement.should.have.property('are', 42);
+			assignement.should.have.property('you', 42);
+		});
+
+		it('should take an object', function() {
+			var assignement = util.commonAssignement({ hello: 'mummy', bye: 'puppy' });
+			assignement.should.be.a('object');
+			assignement.should.have.property('hello', 'mummy');
+			assignement.should.have.property('bye', 'puppy');
+		});
+
+		it('should take an object with depp hierarchy', function() {
+			var assignement = util.commonAssignement({
+				'hello bye': 'puppy'
+			});
+			assignement.should.be.a('object');
+			assignement.should.have.property('hello', 'puppy');
+			assignement.should.have.property('bye', 'puppy');
+		});
+
+		it('should share the same reference assignment', function() {
+			var foo = function() {};
+			var assignement = util.commonAssignement(['hello', 'how', 'are', 'you'], foo);
+			assignement.should.be.a('object');
+			assignement.should.have.property('hello', foo).and.not.eql(function() {});
+			assignement.should.have.property('how', foo).and.not.eql(function() {});
+			assignement.should.have.property('are', foo).and.not.eql(function() {});
+			assignement.should.have.property('you', foo).and.not.eql(function() {});
+		});
+	});
+
+	describe('.multipleAssign', function() {
+		it('should take a simple word as key and a value as assignments', function() {
+			util.multipleAssign('foo', 'bar', function(key, prop, value) {
+				key.should.be.eql('foo');
+				should.not.exist(prop);
+				value.should.be.eql('bar');
+			});
+		});
+
+		it('should take a simple word as key and an object as assignments', function() {
+			var obj = {};
+
+			util.multipleAssign('foo', { bar: 'baz', qux: 666 }, function(key, prop, value) {
+				obj[key] = obj[key] || {};
+				obj[key][prop] = value;
+			});
+
+			obj.should.have.property('foo');
+			obj.should.have.deep.property('foo.bar', 'baz');
+			obj.should.have.deep.property('foo.qux', 666);
+		});
+
+		it('should take a multiple words as key and an object as assignments', function() {
+			var obj = {};
+
+			util.multipleAssign('foo bar', { bar: 'baz', qux: 666 }, function(key, prop, value) {
+				obj[key] = obj[key] || {};
+				obj[key][prop] = value;
+			});
+
+			obj.should.have.property('foo');
+			obj.should.have.deep.property('foo.bar', 'baz');
+			obj.should.have.deep.property('foo.qux', 666);
+			obj.should.have.property('bar');
+			obj.should.have.deep.property('bar.bar', 'baz');
+			obj.should.have.deep.property('bar.qux', 666);
+		});
+
+		it('should take a multiple words as key and an object as assignments and return a composed object when no callback is defined', function() {
+			var obj = util.multipleAssign('foo bar', { bar: 'baz', qux: 666 });
+			obj.should.have.property('foo');
+			obj.should.have.deep.property('foo.bar', 'baz');
+			obj.should.have.deep.property('foo.qux', 666);
+			obj.should.have.property('bar');
+			obj.should.have.deep.property('bar.bar', 'baz');
+			obj.should.have.deep.property('bar.qux', 666);
+		});
+	});
 });
